aa19f47309ba653a1c805fab935534ae5493b81f
agnellogiulio4@gmail.com <agnellogiulio4@gmail.com>
2025-05-28 15:30:23 +0200

    complete_card

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -19,16 +19,22 @@ export default function Main() {
       <div className="container ">
         <div className="row g-2">
           {actors.map((actor) => (
-            <div key={actor.id} className="col-3 ">
-              <div className="card">
+            <div key={actor.id} className="col-3  ">
+              <div className="card vh-100">
                 <img
                   src={actor.image}
-                  className="card-img-top"
+                  className="card-img-top "
                   alt={actor.name}
                 />
                 <div className="card-body">
                   <h5 className="card-title">{actor.name}</h5>
-                  <p className="card-text">{actor.biography}</p>
+                  <hr />
+                  <small>Anno di nascita: {actor.birth_year}</small>
+                  <small> Nazionalità: {actor.nationality}</small>
+                  <hr />
+                  <p className="card-text fs-6">{actor.biography}</p>
+                  <hr />
+                  <small>Awards: {actor.awards}</small>
                 </div>
               </div>
             </div>
