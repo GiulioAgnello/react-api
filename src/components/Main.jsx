fe34cc11df1f265fa15ac0859f25970ec65308e5
agnellogiulio4@gmail.com <agnellogiulio4@gmail.com>
2025-05-28 15:24:35 +0200

    stamp_on_dom

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -3,34 +3,36 @@ import { useState, useEffect } from "react";
 axios;
 const url = "https://lanciweb.github.io/demo/api/actors/";
 
-// presa dei dati
-
-function fetchactor() {
-  axios.get(url).then((res) => {
-    console.log(res.data);
-  });
-}
-
 export default function Main() {
+  // presa dei dati
+  const [actors, setActors] = useState([]);
+
+  function fetchactor() {
+    axios.get(url).then((res) => {
+      setActors(res.data);
+      console.log(res.data);
+    });
+  }
+  useEffect(fetchactor, []);
   return (
     <>
       <div className="container ">
-        <div className="row-cols-3 g-2">
-          <div className="col">
-            <div className="card">
-              <img src="..." className="card-img-top" alt="..." />
-              <div className="card-body">
-                <h5 className="card-title">Card title</h5>
-                <p className="card-text">
-                  Some quick example text to build on the card title and make up
-                  the bulk of the card’s content.
-                </p>
-                <a href="#" className="btn btn-primary">
-                  Go somewhere
-                </a>
+        <div className="row g-2">
+          {actors.map((actor) => (
+            <div key={actor.id} className="col-3 ">
+              <div className="card">
+                <img
+                  src={actor.image}
+                  className="card-img-top"
+                  alt={actor.name}
+                />
+                <div className="card-body">
+                  <h5 className="card-title">{actor.name}</h5>
+                  <p className="card-text">{actor.biography}</p>
+                </div>
               </div>
             </div>
-          </div>
+          ))}
         </div>
       </div>
     </>
